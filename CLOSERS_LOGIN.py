65a8e7a9448b8ef67b8b4b7e7ee324a1e6df60c1
wutim111 <wutim111@gmail.com>
2023-11-18 14:19:03 +0800

    some fix

---
+++ CLOSERS_LOGIN.py
@@ -0,0 +1,25 @@
+import webbrowser    
+import requests
+import json
+
+url = "https://api.closers.com.tw/v1/auth-game/signIn"
+f = open('info.txt')
+lines = f.readlines()
+
+payload = json.dumps({
+  "email": lines[0].replace('\n','').replace('Email:''','').replace('}','').replace('{',''),
+  "password": lines[1].replace('\n','').replace('Password:''','').replace('}','').replace('{',''),
+  "steamid": ""
+})
+headers = {
+  'Content-Type': 'application/json'
+}
+response = requests.request("POST", url, headers=headers, data=payload)
+
+
+response_data = json.loads(response.text)
+result = response_data.get('data')
+access_token=result['userGameInfo']['token']
+
+urL='naddiclaunchertwn:'+access_token
+webbrowser.get('windows-default').open_new(urL)
